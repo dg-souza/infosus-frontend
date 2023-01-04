0dd8043932fcb6247c443eb61d48b9b09e0cc44b
davisg <davi.gama@triadsystems.com.br>
2023-01-04 10:05:35 -0300

    first commit

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,13 @@
+import {
+    MenuContainer
+} from './style'
+
+const Menu = () => {
+    return (
+        <MenuContainer>
+            <h1>aaaaaaaaa</h1>
+        </MenuContainer>
+    )
+}
+
+export default Menu
---
+++ src/pages/Especialidade/index.jsx
@@ -0,0 +1,9 @@
+const Especialidade = () => {
+    return(
+        <>
+            <h1>Especialidades</h1>
+        </>
+    )
+}
+
+export default Especialidade
